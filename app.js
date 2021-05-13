cd59de5130ed3d68ede470eed384c3d04787aa9f
shaoyangyang <yangyang.shao@brgroup.com>
2021-05-13 19:32:50 +0800

    add api-doc

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-require('./db/connect');
+const swaggerUi = require('swagger-ui-express');
 
+const swaggerDocument = require('./swagger.json');
 const userRouter = require('./routes/user');
+require('./db/connect');
 
 const app = express();
 
@@ -14,6 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/user', userRouter);
 
